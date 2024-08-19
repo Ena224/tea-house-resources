b03e92d2bab41f03f1aad40139b2b6e2e22ca423
Ena224 <ena24.nwu@gmail.com>
2024-08-19 10:04:54 +0600

    im so good at pusing code

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// go to tailwind cli then copy the second one.. which is a js library function probably !
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
